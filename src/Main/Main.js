f77c012cf73cdbdd2862d911ee75357c535caf51
Victor Mwenda <kiambi15@gmail.com>
2020-10-09 21:22:44 +0300

    added services folder

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './Main.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Main extends Component{
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+        <div>
+          hello world
+        </div>
+    );
+  }
+
 }
 
-export default App;
+export default Main;
